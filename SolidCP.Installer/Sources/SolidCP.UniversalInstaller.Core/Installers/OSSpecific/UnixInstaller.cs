ea6398a4b08a84d771d9c7be3faf6c08013ca377
Simon Jakob Egli <simon.jakob.egli@gmail.com>
2025-11-13 09:29:29 +0100

    Updated AspNetCoreServer to net10

--- SolidCP.Installer/Sources/SolidCP.UniversalInstaller.Core/Installers/OSSpecific/UnixInstaller.cs
+++ SolidCP.Installer/Sources/SolidCP.UniversalInstaller.Core/Installers/OSSpecific/UnixInstaller.cs
@@ -30,7 +30,7 @@ public abstract class UnixInstaller : Installer
 	{
 		Log.WriteStart("Install AspNetCoreSharedServer");
 
-		const string AspNetCoreSharedServerVersion = "1.2.1";
+		const string AspNetCoreSharedServerVersion = "1.3.0";
 
 		if (installedAspNetCoreSharedServer) return;
 		installedAspNetCoreSharedServer = true;
